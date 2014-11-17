8add41b5d5609bd84b2a8b616333838d24c2a21d
Philipp Jovanovic <philipp@jovanovic.io>
2014-11-17 13:27:13 +0100

    aliases updated

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -5,7 +5,7 @@ alias tmux='tmux -u'
 alias brewer='brew update && brew upgrade && brew cleanup && brew doctor'
 alias getip='curl -s http://icanhazip.com'
 alias grep='grep --color=auto'
-alias sage='/Applications/Sage-6.3.app/Contents/Resources/sage/sage'
+alias sage='/Applications/sage/sage'
 
 if [ -e "$HOME/.aliases-ssh" ]
 then
