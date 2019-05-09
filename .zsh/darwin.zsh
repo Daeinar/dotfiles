bf4c0860b29136a41ad20ff98990b41cdf62274a
Philipp Jovanovic <philipp@jovanovic.io>
2019-05-09 10:09:58 +0200

    ZSH configs

--- .zsh/darwin.zsh
+++ .zsh/darwin.zsh
@@ -1,5 +1,5 @@
 # MacOS-specific configurations
- 
+
 # Homebrew
 export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
@@ -8,18 +8,15 @@ export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 
 # Python
-export PATH=/Users/cosh/Library/Python/3.6/bin:$PATH
+#export PATH=/Users/cosh/Library/Python/3.6/bin:$PATH
 
 # LLVM (from Homebrew)
 export PATH=/usr/local/opt/llvm/bin:$PATH
 
 # Golang
-export GOPATH=~/Code/go
+export GOPATH=/Volumes/Data/code/go
 export PATH=$PATH:$GOPATH/bin
 
 # Rustup
 export PATH=$PATH:$HOME/.cargo/bin
 fpath+=~/.zfunc # auto completion
-
-# Export libary path (required, e.g., for the DFINITY crypto library)
-export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/local/lib
--- .zshrc
+++ .zshrc
@@ -5,17 +5,19 @@ export LC_CTYPE=$LANG
 export ZSH=~/.zsh
 
 # Load platform-specific settings
-case `uname` in
-  Darwin)
-      source $ZSH/os/darwin.zsh
-  ;;
-  Linux)
-      source $ZSH/os/linux.zsh
-  ;;
-esac
+os=`uname | awk '{print tolower($0)}'`
+source $ZSH/$os.zsh
 
-# Load all other ZSH modules
-for config_file ($ZSH/*.zsh) source $config_file
+# Load all other ZSH config files
+configs=(aliases.zsh appearance.zsh \
+         colors.zsh completion.zsh \
+         correction.zsh directories.zsh \
+         history.zsh keybindings.zsh \
+         misc.zsh prompt.zsh \
+         termsupport.zsh)
+for c in $configs ; do
+    source $ZSH/$c
+done
 
 autoload -U compinit
 compinit -i
