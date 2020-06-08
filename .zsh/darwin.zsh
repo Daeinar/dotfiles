8fb1c0145162ef14fffbb18db4e6ed08e326e096
Philipp Jovanovic <philipp@jovanovic.io>
2020-06-08 20:05:23 +0200

    NVM config

--- .zsh/darwin.zsh
+++ .zsh/darwin.zsh
@@ -21,3 +21,27 @@ export PATH=$PATH:$GOPATH/bin
 # Rustup
 export PATH=$PATH:$HOME/.cargo/bin
 fpath+=~/.zfunc # auto completion
+
+# Ruby
+export PATH=/usr/local/opt/ruby/bin:$PATH
+
+# NVM (default)
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
+# NVM (workaround for faster loading); credit: https://www.reddit.com/r/node/comments/4tg5jg/lazy_load_nvm_for_faster_shell_start/d5ib9fs/
+#declare -a NODE_GLOBALS=(`find ~/.nvm/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
+#
+#NODE_GLOBALS+=("node")
+#NODE_GLOBALS+=("nvm")
+#
+#load_nvm () {
+#    export NVM_DIR=~/.nvm
+#    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+#}
+#
+#for cmd in "${NODE_GLOBALS[@]}"; do
+#    eval "${cmd}(){ unset -f ${NODE_GLOBALS}; load_nvm; ${cmd} \$@ }"
+#done
+
